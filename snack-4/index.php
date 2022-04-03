d307132ed10edf3dcace94cf52ad2afabf893f70
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-03 17:39:30 +0200

    Snack 4 completed

--- snack-4/index.php
+++ snack-4/index.php
@@ -1,3 +1,22 @@
 <!-- # Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso
 numero più di una volta. -->
+
+<?php
+
+$numbers = [];
+
+while (count($numbers) < 15) {
+
+    $random_numbers = rand(1, 100);
+
+    if (!in_array($random_numbers, $numbers)) {
+
+        array_push($numbers, $random_numbers);
+    };
+    
+};
+
+var_dump($numbers);
+
+?>
